38c3fc4c42ce88dd75fa9c720612ed640d2c89ae
amneesh <156264154+Amneesh@users.noreply.github.com>
2025-07-10 01:40:12 +0000

    rpd-v2

--- script.js
+++ script.js
@@ -95,3 +95,68 @@ const socialPopup = document.getElementById('socialPopup');
 toggleBtn.addEventListener('click', () => {
   socialPopup.classList.toggle('hidden');
 });
+
+//  const canvas = document.getElementById('banner');
+//   const ctx = canvas.getContext('2d');
+//   const width = canvas.width;
+//   const height = canvas.height;
+
+//   class LightBeam {
+//     constructor(x, width, speed, color) {
+//       this.x = x;
+//       this.y = -200; // start above canvas
+//       this.width = width;
+//       this.speed = speed;
+//       this.color = color;
+//       this.height = 200;
+//     }
+
+//     update() {
+//       this.y += this.speed;
+//       if (this.y > height) {
+//         this.y = -this.height;
+//       }
+//     }
+
+//     draw(ctx) {
+//       const gradient = ctx.createLinearGradient(this.x, this.y, this.x, this.y + this.height);
+//       gradient.addColorStop(0, this.color + '80'); // semi-transparent start
+//       gradient.addColorStop(0.5, this.color + 'cc'); // brighter middle
+//       gradient.addColorStop(1, this.color + '00'); // transparent end
+
+//       ctx.fillStyle = gradient;
+//       ctx.fillRect(this.x, this.y, this.width, this.height);
+//     }
+//   }
+
+//   // Create an array of beams
+//   const beams = [];
+//   const colors = ['#FF69B4', '#800080']; // pink and purple hex colors
+
+//   for(let i = 0; i < 12; i++) {
+//     const x = i * 160 + Math.random() * 40; // spread beams horizontally with some randomness
+//     const width = 20 + Math.random() * 10;
+//     const speed = 1 + Math.random() * 1.5;
+//     const color = colors[i % colors.length];
+//     beams.push(new LightBeam(x, width, speed, color));
+//   }
+
+//   function animate() {
+//     ctx.clearRect(0, 0, width, height);
+
+//     // Draw black gradient background from bottom
+//     const bgGradient = ctx.createLinearGradient(0, height, 0, 0);
+//     bgGradient.addColorStop(0, 'black');
+//     bgGradient.addColorStop(0.7, 'rgba(0,0,0,0)');
+//     ctx.fillStyle = bgGradient;
+//     ctx.fillRect(0, 0, width, height);
+
+//     beams.forEach(beam => {
+//       beam.update();
+//       beam.draw(ctx);
+//     });
+
+//     requestAnimationFrame(animate);
+//   }
+
+//   animate();
